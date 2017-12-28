ce631873c0ab0b7cfa4bff853f80b05a3f2b8fb9
denidiasjr <denidias96@gmail.com>
2017-12-28 13:09:01 -0300

    First commit!

---
+++ modules.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    sairDaJaula: function(nome){
+        return 'Tá saindo da jaula o '+nome;
+    },
+    irineu: function(){
+        return 'Você não sabe e nem eu';
+    }
+    
+}
