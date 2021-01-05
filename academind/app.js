b8732bf99221f18b615250866f859def881ed88b
Deni Junior <denidiasjr@gmail.com>
2021-01-04 22:29:54 -0300

    Playing with res object!

--- academind/app.js
+++ academind/app.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+
+  res.setHeader('Content-type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My page written on Node.js</title></head>');
+  res.write('<body>');
+  res.write('<h1>Welcome to my page, stranger</h1>');
+  res.write('<p>Hope you enjoy it!</p>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
