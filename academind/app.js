236eff71a1ecc66a2aa8b162ddc4274391f5394b
Deni Junior <denidiasjr@gmail.com>
2021-01-05 23:36:37 -0300

    Splitting urls treatment

--- academind/app.js
+++ academind/app.js
@@ -1,8 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const {
+    url,
+    method
+  } = req;
 
+  if (url === '/') {
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write('<body>');
+    res.write('<form action="/message" method="POST">');
+    res.write('<input type="text" name="message" />');
+    res.write('<button type="submit">Send</button>');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', "MEU TEXTO TOP");
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  
   res.setHeader('Content-type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My page written on Node.js</title></head>');
