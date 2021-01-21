776654e12be3f17fd6bf3fb4f386311e3846afa2
Deni Junior <denidiasjr@gmail.com>
2021-01-20 21:07:44 -0300

    Salvando o arquivo message.txt de forma assíncrona

--- academind/app.js
+++ academind/app.js
@@ -28,16 +28,17 @@ const server = http.createServer((req, res) => {
       body.push(chunk);
     });
     
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
       console.log({ body, parsedBody, message });
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
 
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
   
   res.setHeader('Content-type', 'text/html');
