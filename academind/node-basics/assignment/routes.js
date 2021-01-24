f14b2a6dd42b35cc5e7fa4f3d6803ce161413f88
Deni Junior <denidiasjr@gmail.com>
2021-01-24 12:00:40 -0300

    Finishing assignment

--- academind/node-basics/assignment/routes.js
+++ academind/node-basics/assignment/routes.js
@@ -11,6 +11,10 @@ const requestHandler = (req, res) => {
     res.write('<head><title>Greeting page</title></head>');
     res.write('<body>');
     res.write('<h1>Hello my friend!</h1>')
+    res.write('<form method="POST" action="/create-user">')
+    res.write('<input type="text" name="username" />')
+    res.write('<button type="submit">Register</button>')
+    res.write('</form>')
     res.write('</body>');
     res.write('</html>');
     return res.end();
@@ -29,7 +33,35 @@ const requestHandler = (req, res) => {
     res.write('</ul>');
     res.write('</body>');
     res.write('</html>');
-    res.end();
+    return res.end();
+  }
+
+  if (url === '/create-user' && method === 'POST') {
+    const body = [];
+
+    req.on('data', chunk => {
+      body.push(chunk);
+    })
+
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const username = parsedBody.split('=')[1];
+      console.log({ username });
+      res.setHeader('Content-type', 'text/html');
+      res.write('<html>');
+      res.write('<head><title>User registered successfully</title></head>');
+      res.write('<body>');
+      res.write('<h1>Users</h1>')
+      res.write('<ul>');
+      res.write('<li>Periclao</li>');
+      res.write('<li>Zeca Pagodinho</li>');
+      res.write('<li>Salgadinho</li>');
+      res.write(`<li>${username}</li>`);
+      res.write('</ul>');
+      res.write('</body>');
+      res.write('</html>');
+      return res.end();
+    })
   }
 }
 
