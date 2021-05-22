f2ccc95f7998d5d936e3999297a7e43792ed2bc6
Deni Junior <denidiasjr@gmail.com>
2021-05-22 16:46:28 -0300

    First challenge of express

--- academind/express/app.js
+++ academind/express/app.js
@@ -3,14 +3,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next();
+app.use('/users', (req, res, next) => {
+  console.log('/users middleware');
+  res.send('<h1>Here should be the users list</h1>')
 })
 
-app.use((req, res, next) => {
-  console.log('In another the middleware!');
-  res.send('<h1>Hello from Deni\'s Express server!</h1>')
+app.use('/', (req, res, next) => {
+  console.log('/ middleware');
+  res.send('<h1>Here we should have the Homepage</h1>')
 })
 
 const server = http.createServer(app);
